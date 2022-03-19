b959c568dc162d7762c965ef26bd8feb14aeed6b
avimehenwal <avi.mehanwal@gmail.com>
2022-03-20 00:21:42 +0100

    docs: add donations research

---
+++ docs/helpUkraine_research.mm
@@ -0,0 +1,191 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="digital fundraising" FOLDED="false" ID="ID_191153586" CREATED="1647728059215" MODIFIED="1647730989602" ICON_SIZE="36.0 pt" LINK="menuitem:_ExternalImageAddAction" STYLE="oval">
+<font SIZE="22"/>
+<hook NAME="MapStyle">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" ICON_SIZE="64.0 pt" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font NAME="Segoe Print" SIZE="22"/>
+<edge COLOR="#ffffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" ICON_SIZE="32.0 px" COLOR="#000000" BACKGROUND_COLOR="#ffffcc" STYLE="bubble" SHAPE_HORIZONTAL_MARGIN="2.6 pt" SHAPE_VERTICAL_MARGIN="2.6 pt" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="18" BOLD="false" ITALIC="true"/>
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" ICON_SIZE="28.0 px" COLOR="#000000" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="16"/>
+<edge STYLE="bezier" WIDTH="4"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" ICON_SIZE="24.0 px" COLOR="#000000" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="14"/>
+<edge STYLE="bezier" WIDTH="3"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" ICON_SIZE="24.0 px" COLOR="#111111" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="13"/>
+<edge STYLE="bezier" WIDTH="2"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5" ICON_SIZE="24.0 px" BORDER_WIDTH_LIKE_EDGE="true">
+<font SIZE="12"/>
+<edge STYLE="bezier" WIDTH="1"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6" ICON_SIZE="24.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7" ICON_SIZE="16.0 px">
+<font SIZE="10"/>
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11" ICON_SIZE="16.0 px">
+<edge STYLE="bezier"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="22" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
+<edge COLOR="#ffffff"/>
+<richcontent TYPE="DETAILS">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      digitial fundraising ideas
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="who" POSITION="right" ID="ID_90823870" CREATED="1647728059238" MODIFIED="1647730676822" HGAP_QUANTITY="-316.60364546709275 pt" VSHIFT_QUANTITY="76.53657839381219 pt">
+<edge COLOR="#ff0000"/>
+<node TEXT="people, services" ID="ID_1201198400" CREATED="1647728059239" MODIFIED="1647728113279" HGAP_QUANTITY="15.756097642670499 pt" VSHIFT_QUANTITY="-15.804878784034486 pt"/>
+<node TEXT="infrastructure" ID="ID_1981327430" CREATED="1647728113895" MODIFIED="1647728567880" HGAP_QUANTITY="16.249999932944775 pt" VSHIFT_QUANTITY="5.249999843537812 pt"/>
+</node>
+<node TEXT="How?" POSITION="left" ID="ID_25434398" CREATED="1647728059239" MODIFIED="1647729757021" HGAP_QUANTITY="81.20731962727953 pt" VSHIFT_QUANTITY="-80.50609804336646 pt">
+<edge COLOR="#cc00ff"/>
+<node TEXT="Supplies" ID="ID_352937383" CREATED="1647728059239" MODIFIED="1647728456490" HGAP_QUANTITY="13.121951178664752 pt" VSHIFT_QUANTITY="-9.658537034687741 pt"/>
+<node TEXT="donations" ID="ID_1814832351" CREATED="1647728456959" MODIFIED="1647730551822">
+<node TEXT="sources" ID="ID_847156617" CREATED="1647728761756" MODIFIED="1647728765867">
+<node TEXT="inbound" ID="ID_1591500768" CREATED="1647728802842" MODIFIED="1647728806457"/>
+<node TEXT="outbound" ID="ID_583962372" CREATED="1647728806889" MODIFIED="1647728810079"/>
+<node TEXT="official and government" ID="ID_101049611" CREATED="1647728811967" MODIFIED="1647728819754"/>
+<node TEXT="NGO owned" ID="ID_288920980" CREATED="1647728820373" MODIFIED="1647728827810"/>
+<node TEXT="gifts" ID="ID_1663958328" CREATED="1647730411425" MODIFIED="1647730413614"/>
+</node>
+</node>
+<node TEXT="Petitions" ID="ID_966834988" CREATED="1647728059239" MODIFIED="1647728472232" HGAP_QUANTITY="15.756097642670499 pt" VSHIFT_QUANTITY="-7.902439392017243 pt"/>
+<node TEXT="charity and NGOs" ID="ID_1865004345" CREATED="1647728472842" MODIFIED="1647728480159"/>
+</node>
+<node TEXT="supplies/Aids" POSITION="right" ID="ID_114569843" CREATED="1647728163123" MODIFIED="1647728560192" HGAP_QUANTITY="22.999999731779106 pt" VSHIFT_QUANTITY="-170.24999492615476 pt">
+<edge COLOR="#00ffff"/>
+<node TEXT="Financial Aid" ID="ID_79907121" CREATED="1647728180511" MODIFIED="1647728187723">
+<node TEXT="for Military" ID="ID_950306652" CREATED="1647728188921" MODIFIED="1647728194025"/>
+<node TEXT="for medial Services" ID="ID_286911360" CREATED="1647728194408" MODIFIED="1647728201554"/>
+<node TEXT="food and clothing" ID="ID_1884308098" CREATED="1647728205204" MODIFIED="1647728211442"/>
+<node TEXT="shelter" ID="ID_1948482400" CREATED="1647728211804" MODIFIED="1647728215100"/>
+<node TEXT="water" ID="ID_1227730727" CREATED="1647728215401" MODIFIED="1647728217799"/>
+<node TEXT="electricity" ID="ID_603084052" CREATED="1647728217964" MODIFIED="1647728223524"/>
+</node>
+<node TEXT="Relief Supplies" ID="ID_1854440247" CREATED="1647728232519" MODIFIED="1647728237990"/>
+<node TEXT="supplies for War and defence" ID="ID_1976481425" CREATED="1647728240843" MODIFIED="1647728439824"/>
+<node TEXT="monetary aids" ID="ID_888744797" CREATED="1647730573964" MODIFIED="1647730582527">
+<node TEXT="one-time" ID="ID_1165487860" CREATED="1647730583906" MODIFIED="1647730586730"/>
+<node TEXT="recurring" ID="ID_1057124623" CREATED="1647730587088" MODIFIED="1647730590244"/>
+<node TEXT="micro-donations" ID="ID_69409348" CREATED="1647730604589" MODIFIED="1647730614833"/>
+<node TEXT="gift economy" ID="ID_236216331" CREATED="1647730615131" MODIFIED="1647730618987"/>
+<node TEXT="philanthrophy" ID="ID_1342872189" CREATED="1647730619335" MODIFIED="1647730627654"/>
+<node TEXT="cash/assets" ID="ID_674345055" CREATED="1647730641687" MODIFIED="1647730649434"/>
+</node>
+</node>
+<node TEXT="Accommodation" POSITION="left" ID="ID_934704954" CREATED="1647728254820" MODIFIED="1647729749804" HGAP_QUANTITY="110.74999711662541 pt" VSHIFT_QUANTITY="-104.24999689310798 pt">
+<edge COLOR="#7c0000"/>
+<node TEXT="for refugees" ID="ID_1891316356" CREATED="1647728267147" MODIFIED="1647728271358"/>
+<node TEXT="for Host" ID="ID_1144477886" CREATED="1647728271717" MODIFIED="1647728277900"/>
+</node>
+<node TEXT="statistics/data" POSITION="right" ID="ID_729388389" CREATED="1647728483238" MODIFIED="1647730067987" HGAP_QUANTITY="166.24999546259656 pt" VSHIFT_QUANTITY="-186.74999443441646 pt">
+<edge COLOR="#00007c"/>
+<node TEXT="who is affected?" ID="ID_463528991" CREATED="1647728493755" MODIFIED="1647728504077"/>
+<node TEXT="scale of loss" ID="ID_791515073" CREATED="1647728504439" MODIFIED="1647728508913">
+<node TEXT="human" ID="ID_400364017" CREATED="1647728513324" MODIFIED="1647728515495"/>
+<node TEXT="infrastructure" ID="ID_1958601903" CREATED="1647728515740" MODIFIED="1647728519622"/>
+<node TEXT="widlife and nature" ID="ID_1095420034" CREATED="1647728519965" MODIFIED="1647728525360"/>
+<node TEXT="natural resource" ID="ID_1473130369" CREATED="1647728525522" MODIFIED="1647728531753"/>
+</node>
+<node TEXT="total lives lost" ID="ID_173210745" CREATED="1647728539765" MODIFIED="1647728549968"/>
+</node>
+<node TEXT="News sources" POSITION="left" ID="ID_1613340583" CREATED="1647729713775" MODIFIED="1647729762044" HGAP_QUANTITY="88.99999776482589 pt" VSHIFT_QUANTITY="0.0 pt">
+<edge COLOR="#007c00"/>
+<node TEXT="twitter" ID="ID_1036733615" CREATED="1647729726129" MODIFIED="1647729729062"/>
+<node TEXT="google news" ID="ID_262893889" CREATED="1647729729396" MODIFIED="1647729732608"/>
+<node TEXT="kvive news" ID="ID_640520097" CREATED="1647729733107" MODIFIED="1647729736861"/>
+<node TEXT="russian media news" ID="ID_1092766934" CREATED="1647729737229" MODIFIED="1647729742384"/>
+</node>
+<node TEXT="Lingo" POSITION="right" ID="ID_439831221" CREATED="1647730032300" MODIFIED="1647730072349" HGAP_QUANTITY="172.99999526143088 pt" VSHIFT_QUANTITY="-82.49999754130847 pt">
+<edge COLOR="#7c007c"/>
+<node TEXT="fundraising campaigns" ID="ID_1831616315" CREATED="1647730038267" MODIFIED="1647730049700">
+<node TEXT="successful fundraising techniques" ID="ID_421573795" CREATED="1647730132780" MODIFIED="1647730143067"/>
+</node>
+<node TEXT="crowdfunding" ID="ID_1365615065" CREATED="1647730080206" MODIFIED="1647730084582"/>
+</node>
+<node TEXT="fundraising participants" POSITION="left" ID="ID_22098174" CREATED="1647730921963" MODIFIED="1647730989601" HGAP_QUANTITY="-114.99999615550053 pt" VSHIFT_QUANTITY="81.74999756366022 pt">
+<edge COLOR="#007c7c"/>
+<node TEXT="active participants who contributed to the cause" ID="ID_556708296" CREATED="1647730938793" MODIFIED="1647730950041">
+<node TEXT="hall of fame" ID="ID_1809783489" CREATED="1647730952322" MODIFIED="1647730955639"/>
+</node>
+<node TEXT="potential participants which could converge" ID="ID_1414646884" CREATED="1647730960577" MODIFIED="1647730974346"/>
+<node TEXT="expansion pool participants" ID="ID_888399873" CREATED="1647730975191" MODIFIED="1647730981730"/>
+</node>
+<node TEXT="Marketting and Sales" POSITION="right" ID="ID_203827435" CREATED="1647731022924" MODIFIED="1647731030691">
+<edge COLOR="#7c7c00"/>
+<node TEXT="how to promote and outreach people" ID="ID_1376408188" CREATED="1647731092566" MODIFIED="1647731112237"/>
+<node TEXT="how to make an impact" ID="ID_696870186" CREATED="1647731112827" MODIFIED="1647731117753">
+<node TEXT="call to action" ID="ID_1343193127" CREATED="1647731123238" MODIFIED="1647731126951"/>
+</node>
+</node>
+</node>
+</map>
