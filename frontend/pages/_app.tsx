d88afbc96e24a525bb049275205d1af6d0ee9305
avimehenwal <avi.mehanwal@gmail.com>
2022-03-20 16:26:00 +0100

    feat(graphql): wirein graphql client to nextjs

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import {
+  ApolloClient, ApolloProvider, InMemoryCache
+} from "@apollo/client";
+import type { AppProps } from 'next/app';
+import React from 'react';
+import '../styles/globals.css';
+
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/api/graphql',
+  cache: new InMemoryCache()
+});
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
